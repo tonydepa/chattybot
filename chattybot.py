f7ad2aebeae03eafb2b5bcd1eb5570b052b52be3
tonydepa <69813395+tonydepa@users.noreply.github.com>
2020-09-14 12:17:16 -0700

    Create chattybot.py

---
+++ chattybot.py
@@ -0,0 +1,54 @@
+def greet():
+    print('Hello! My name is R2D2')
+    print('I was created in 2020')
+
+
+def remind_name():
+    print('Please, remind me your name.')
+    name = input()
+    print('What a great name you have, ' + name + '!')
+
+
+def guess_age():
+    print('Let me guess your age.')
+    print('Enter remainders of dividing your age by 3, 5 and 7.')
+
+    rem3 = int(input())
+    rem5 = int(input())
+    rem7 = int(input())
+    age = (rem3 * 70 + rem5 * 21 + rem7 * 15) % 105
+
+    print("Your age is " + str(age) + "; that's a good time to start programming!")
+
+
+def count():
+    print('Now I will prove to you that I can count to any number you want.')
+
+    num = int(input())
+    curr = 0
+    while curr <= num:
+        print(curr, '!')
+        curr = curr + 1
+
+def test():
+    print("Let's test your programming knowledge.")
+    print("Why do we use methods?")
+    print("1. To repeat a statement multiple times.")
+    print("2. To decompose a program into several small subroutines.")
+    print("3. To determine the execution time of a program.")
+    print("4. To interrupt the execution of a program.")
+    anwser = int(input())
+    while anwser != 2:
+        print("Please, try again.")
+        anwser = int(input())
+    print("Completed, have a nice day!")
+    
+def end():
+    print('Congratulations, have a nice day!')
+    
+greet()
+remind_name()
+guess_age()
+count()
+test()
+end()
